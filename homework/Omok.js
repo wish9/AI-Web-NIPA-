78a0cdf5c40ecc206baffbeebe9ab921a681dab0
wish9 <wjwee9@gmail.com>
2023-07-26 01:14:13 +0900

    fix: 예외처리 기능분리(+예외처리 추가)

--- homework/Omok.js
+++ homework/Omok.js
@@ -94,37 +94,50 @@ function overRange(x, y) {
     return false;
 }
 
+// 입력값에 대한 예외처리
+function validateInput(inputValue) {
+    let coords = inputValue.split(',');
+    let x = parseInt(coords[0]);
+    let y = parseInt(coords[1]);
+
+    if(isNaN(x) || isNaN(y)) {
+        console.log("숫자를 입력해주세요.");
+        return null;
+    }
+
+    if(x < 0 || x >= 30 || y < 0 || y >= 30) {
+        console.log("0이상 30미만 값을 입력해주세요.");
+        return null;
+    }
+
+    if(arr[x][y] !== '') {
+        if(arr[x][y] === '흑' || arr[x][y] === '백') {
+            console.log("이미 돌을 놓은 자리입니다.");
+        }else {
+            console.log("올바른 좌표값을 입력하세요.");
+            console.log("예시: 1,3 or 1, 3 or 10, 15 등등");
+            console.log("잘못된 예시: [1,3] or 1,     2 or 1.2");
+        }
+        return null;
+    }
+
+    return [x, y];
+}
+
 
 /*
     입력값 받아서 보드에 넣는 실질적인 게임 메서드
-    + 예외처리
 */
 function play() {
     let player = turn % 2 == 0 ? '백' : '흑'; // w for white, b for black
     rl.question(`${player === '흑' ? '흑' : '백'}색 돌을 놓을 좌표를 입력하세요 :`, (inputValue) => {
-        let coords = inputValue.split(',');
+        let coords = validateInput(inputValue);
+        if(coords === null) {
+            play();
+            return;
+        }
         let x = parseInt(coords[0]);
         let y = parseInt(coords[1]);
-
-        // 입력값 범위 초과에 대한 예외처리
-        if(x < 0 || x >= 30 || y < 0 || y >= 30) {
-            console.log("0이상 30미만 값을 입력해주세요.");
-            play();
-            return;
-        }
-
-        // 이미 둔 자리 or 잘못된 입력양식에 대한 예외처리
-        if(arr[x][y] !== '') {
-            if(arr[x][y] === '흑' || arr[x][y] === '백') {
-                console.log("이미 돌을 놓은 자리입니다.");
-            }else {
-                console.log("올바른 좌표값을 입력하세요.");
-                console.log("예시: 1,3 or 1, 3 or 10, 15 등등");
-                console.log("잘못된 예시: [1,3] or 1,     2 or 1.2");
-            }
-            play();
-            return;
-        }
 
         arr[x][y] = player;
         printBoard();
